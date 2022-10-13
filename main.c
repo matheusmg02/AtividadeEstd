f2976684fe77eef0707698a10d37eabb48fbb361
Matheus Macedo <matheusmacedo@BL3-LABVICIA-12.maracanau.ifce.edu.br>
2022-10-13 11:40:55 -0300

    refatoração: mudanças de alguns nomes

--- main.c
+++ main.c
@@ -6,88 +6,115 @@ typedef struct aux
 {
     float value;
     struct aux *next;
-}Enum;
+} No;
 
-float operation(float a, float b, char c){
-    switch (c)
+float operate(float a, float b, char operator)
+{
+    switch (operator)
     {
     case '+':
-        return a+b;
+        return a + b;
         break;
     case '-':
-        return a-b;
+        return a - b;
         break;
     case '*':
-        return a*b;
+        return a * b;
         break;
     case '/':
-        return a/b;
+        return a / b;
         break;
-    
+
     default:
         return 0.0;
     }
 }
 
-Enum *stack(float num, Enum *list){
-    Enum *new = malloc(sizeof(Enum));
+No *insert_in_stack(float num, No *list)
+{
+    No *new = malloc(sizeof(No));
 
-    if(new){
+    if (new)
+    {
         new->value = num;
         new->next = list;
 
         return new;
-    }else{
+    }
+    else
+    {
         printf("Erro ao alocar memória!");
     }
     return NULL;
 }
 
-Enum *unstack(Enum **list){
-    Enum *remove = NULL;
+No *remove_from_stack(No **list)
+{
+    No *remove = NULL;
 
-    if(*list){
+    if (*list)
+    {
         remove = *list;
         *list = remove->next;
-    }else{
+    }
+    else
+    {
         printf("Expressão inválida");
     }
     return remove;
 }
 
-float solve_expression( char c[]){
-    char *pt;
+float solve_expression(char expression[])
+{
+    char *element;
     float result;
-    Enum *n1,*n2,*list = NULL;
-    pt=strtok(c," ");
-    while(pt){
-        if(pt[0]=='+'|| pt[0]=='-'|| pt[0]=='*'|| pt[0]=='/'){
-            n1=unstack(&list);
-            n2=unstack(&list);
-            result = operation(n2->value, n1->value, pt[0]);
-            list = stack( result, list);
+    No *n1, *n2;
+    No *list = NULL;
+    element = strtok(expression, " ");
+
+    // pointer[0] é um operador
+
+    while (element != NULL)
+    {
+        if (element[0] == '+' || element[0] == '-' || element[0] == '*' || element[0] == '/')
+        {
+            n1 = remove_from_stack(&list);
+            n2 = remove_from_stack(&list);
+            result = operate(n2->value, n1->value, element[0]);
+            list = insert_in_stack(result, list);
             free(n1);
             free(n2);
-        }else{
-            result = strtol(pt, NULL, 10);
-             list = stack( result, list);
         }
-        pt = strtok(NULL, " ");
+        else
+        {
+            result = strtol(element, NULL, 10); // string to long
+            list = insert_in_stack(result, list);
+        }
+        element = strtok(NULL, " ");
     }
-    n1 = unstack(&list);
-    result = n1 ->value;
+    n1 = remove_from_stack(&list);
+    result = n1->value;
     free(n1);
+
     return result;
 }
 
- void main()
+void main()
 {
     char expression[200];
     printf("Digite aqui sua expressão posfixada: ");
-    fgets(expression, 200,stdin);
+    fgets(expression, 200, stdin);
     printf("\n resultado:%f\n", solve_expression(expression));
-    
 }
+
+// "20 30 + 7 4 - 9 50 * 9 8 +"
+// "20" "30 + 7 4 - 9 50 * 9 8 +"
+// "20" "30" "+ 7 4 - 9 50 * 9 8 +"
+// "20" "30" "+" "7 4 - 9 50 * 9 8 +"
+// .......
+// "20" "30" "+" "7" "4" "-" "9" "50" "*" "9" "8 +"
+// "20" "30" "+" "7" "4" "-" "9" "50" "*" "9" "8" "+"
+// NULL = '\0'
 
 /*
 20 30 + 7 4 - 9 50 * 9 8 + = 17
